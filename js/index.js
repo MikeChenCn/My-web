e622208701dc609c6e365df5e9c667fcfb3d70c9
MikeChenCn <7900670714@qq.com>
2018-11-23 21:36:02 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 	$('#myFullpage').fullpage({
 		continuousVertical:'true',
 		anchors: ['page1', 'page2', 'page3', 'page4'],
-		menu:'#menu'
+		menu: '#menu'
 	});
 });
